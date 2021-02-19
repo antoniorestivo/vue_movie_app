569180ca4fb4cea9e90404f536ebd8a17d3716f3
antoniorestivo <nonebesides370@gmail.com>
2021-02-19 09:41:55 -0700

    Partially done homework

--- src/views/MovieIndex.vue
+++ src/views/MovieIndex.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="movies-index">
     <h1>{{ message }}</h1>
-    <div v-for="movie in movies" v-bind:key="movie.id">
+    <div>Search by title: <input v-model="titleFilter">
+    Search by name: <input v-model="titleFilter" list="titles">
+<datalist id="titles">
+	<option v-for="movie in movies" v-bind:key="movie.id">{{ movie.title }}</option>
+</datalist>
+    <div v-for="movie in filterBy(movies, titleFilter, 'title')" v-bind:key="movie.id">
+
+      
       <h2>Movie Title: {{movie.title}}</h2>
       <h3>Movie Year: {{movie.year}}</h3>
       <h4>Movie Plot: {{movie.plot}}</h4>
@@ -10,16 +17,21 @@
 
   </div>
   </div>
+  </div>
 </template>
 <style></style>
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function() {
     return {
       message: "Movie List",
       movies:[],
+      titleFilter: "",
+
       
     };
   },
